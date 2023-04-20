3f89321842a6f0be49ac254dd7c40e4fa3efeacc
notpallav <43105309+notpallav@users.noreply.github.com>
2023-04-19 18:25:51 -0700

    add document class

---
+++ src/backend/db/document.cpp
@@ -0,0 +1,19 @@
+#include "document.h"
+
+Document::Document(int id) {
+    this->id = id;
+}
+
+int Document::get_id() {
+    return id;
+}
+
+std::string Document::get_content() {
+    return body;
+}
+
+bool Document::update_content(std::string content) {
+    body = content;
+    // will return false in the future if the given content is not valid json
+    return true;
+}
---
+++ src/backend/db/document.h
@@ -0,0 +1,21 @@
+#ifndef DOCUMENT_H
+#define DOCUMENT_H
+
+#include <string>
+#include <vector>
+
+class Document {
+   public:
+    Document(int id);
+    int get_id();
+    std::string get_content();
+    bool update_content(std::string content);
+
+   private:
+    int id;
+
+    // this will be a json object in the future
+    std::string body;
+};
+
+#endif
