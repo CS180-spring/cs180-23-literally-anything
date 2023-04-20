48594f718522553b071214916487be8faf5b970d
notpallav <43105309+notpallav@users.noreply.github.com>
2023-04-20 14:19:28 -0700

    db

---
+++ src/backend/db/database.cpp
@@ -0,0 +1,30 @@
+#include "database.h"
+
+#include <iostream>
+using namespace std;
+Database::Database() {
+    this->name = "default_db";
+    this->id = 99999;
+}
+Database::Database(int d, string name) {
+    this->name = name;
+}
+bool Database::create_collection(int id, string name) {
+    collections.insert({id, Collection(id, name)});
+    return 1;
+}
+Collection& Database::get_collection(int id) {
+    return collections[id];
+}
+
+map<int, Collection>& Database::get_collections() {
+    return collections;
+}
+
+string Database::get_name() {
+    return name;
+}
+
+int Database::get_id() {
+    return id;
+}
---
+++ src/backend/db/database.h
@@ -0,0 +1,25 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <string>
+#include <vector>
+
+#include "collection.h"
+
+class Database {
+   public:
+    Database();               
+    Database(int id, string name);                // database id
+    bool create_collection(int id, string name);  //  collection id
+    Collection& get_collection(int id);
+    map<int, Collection>& get_collections();
+    string get_name();
+    int get_id();
+
+   private:
+    string name;
+    int id;
+    map<int, Collection> collections;
+};
+
+#endif
