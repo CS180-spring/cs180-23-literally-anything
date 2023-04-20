e02d33a160ccb7c3a266ce3cb70d225790ae08da
Brocks816 <91571357+Brocks816@users.noreply.github.com>
2023-04-20 15:36:31 -0700

    Adding ViewDataBase page and updating App.js

---
+++ src/Pages/ViewDataBase.js
@@ -0,0 +1,11 @@
+import Header from '../Components/Header'
+import { Route, useNavigate, Link, Routes } from "react-router-dom";
+
+export default function ViewDataBase() {
+    return (
+        <>
+            <Header />
+            <h2> View Database Page </h2>
+        </>
+    )
+}
