13117fb26702838da473189e0de4788fc5f8040f
Brocks816 <91571357+Brocks816@users.noreply.github.com>
2023-04-27 14:31:29 -0700

    Adding CRUD buttons to viewdatabase page

--- src/Pages/ViewDataBase.js
+++ src/Pages/ViewDataBase.js
@@ -1,11 +1,22 @@
 import Header from '../Components/Header'
 import { Route, useNavigate, Link, Routes } from "react-router-dom";
 
+
 export default function ViewDataBase() {
+
     return (
         <>
             <Header />
-            <h2> View Database Page </h2>
+            <h2 style={{color: "white"}}> View Database Page </h2>
+            <button
+                style={{ backgroundColor: "#8A5AA4", color: "white", height: '60px', width: '100px' }}
+                > Update Database
+            </button>
+            <button
+                style={{ backgroundColor: "#8A5AA4", color: "white", height: '60px', width: '100px' }}
+                > Delete Database
+            </button>
+ 
         </>
     )
 }
