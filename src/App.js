ad97617ed57278956c9ae430daf115fa07e916d8
Brock Stuckey <bstuc003@ucr.edu>
2023-05-04 11:07:36 -0700

    updating files

--- src/App.js
+++ src/App.js
@@ -4,25 +4,36 @@ import Home from './Pages/Home'
 import CreateDataBase from './Pages/CreateDataBase'
 import ViewDataBase from './Pages/ViewDataBase';
 import NoPage from './Pages/NoPage';
+import axios from 'axios';
+import { React, useEffect, useState } from 'react';
+import Components from './Components/posts'
 
 
 
 function App() {
 
+  const getDB = () => {
+    axios.get('http://54.183.24.168:4000/listDBs').then(
+      (response) => {
+      console.log(response);
+      }
+    );
+  };  
 
   return (
+    
     <div className="App">
       <HashRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/createDataBase" element={<CreateDataBase />} />
-          <Route path="/viewDataBase" element ={<ViewDataBase />} />
+          <Route path="/home/*" element={<Home />} />
+          <Route path="/createDataBase/*" element={<CreateDataBase />} />
+          <Route path="/viewDataBase/*" element ={<ViewDataBase />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </HashRouter>
-
     </div>
+    
   );
 }
 
