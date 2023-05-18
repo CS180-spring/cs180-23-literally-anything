d0daefeafc61a4c5f813dd2691dade7d16f1f58c
gsinghd <gdhal005@ucr.edu>
2023-05-18 12:37:05 -0700

    documents class

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route, HashRouter } from 'react-router-dom';
 // import Home from './Pages/Home'
 import ViewDataBase from './Pages/ViewDataBase';
+import Documents from './Pages/Documents';
 import NoPage from './Pages/NoPage';
 import axios from 'axios';
 import Collections from './Pages/Collections';
@@ -17,8 +18,10 @@ function App() {
         <Routes>
           <Route index element={<ViewDataBase />}/>
           <Route index element={<Collections />} />
+          <Route index element={<Documents />} />
           <Route path="/viewDataBase" element={<ViewDataBase />} />
           <Route path="/Collections" element={<Collections />} />
+          <Route path="/Documents" element={<Documents />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </HashRouter>
---
+++ src/Pages/Documents.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Button } from "../Components/Button";
+import { useNavigate } from "react-router-dom";
+import ViewDataBase from './ViewDataBase';
+import './Documents.css'
+
+const Documents = () => {
+  const [databases, setDatabases] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://54.177.181.151:4000/listDBs")
+      .then((response) => {
+        setDatabases(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const tdStyle = {
+    textAlign: 'center'
+  };
+
+  const thStyle = {
+    textAlign: 'center'
+  };
+
+  return (
+    <div className="documents-container">
+      <div className="sidebar" id = "sidebar">
+        <h2>Databases</h2>
+        <ul>
+          {databases.map(database => (
+            <li key={database.id}>{database.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="content">
+        <div className="table">
+          <table>
+            <thead>
+              <tr>
+                <th>DataBase ID</th>
+                <th style={thStyle}>DataBase Name</th>
+                <th style={thStyle}>View Collections or Delete</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Documents;
