c7e62db14d1fa58ebf6976d785eb26f06e6abdb0
Brock816 <brockstuckey@gmail.com>
2023-05-23 08:30:23 -0700

    Reverting dropdown menu

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,38 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Dropdown.css';
 import { Button } from './Button';
+import axios from 'axios';
 
-
-export const Dropdown = () => {
-  const [dropdownValue, setDropdownValue] = useState('');
-  const [listItems, setListItems] = useState([]);
-
-  const handleDropdownChange = (event) => {
-    setDropdownValue(event.target.value);
-  };
-
-  const handleButtonClick = () => {
-    if (dropdownValue !== '') {
-      setListItems([...listItems, dropdownValue]);
-      setDropdownValue('');
+export const Dropdown =()=> {
+    const [state, setstate] = useState(false);
+    const showDropdown=()=> {
+        setstate(true);
     }
-  };
+    const hideDropdown=()=> {
+        setstate(false);
+    }
+const [data, setData] = useState([]);
 
-  return (
-    <div>
-      <select value={dropdownValue} onChange={handleDropdownChange}>
-        <option value="">Select an item</option>
-        <option value="Item 1">Item 1</option>
-        <option value="Item 2">Item 2</option>
-        <option value="Item 3">Item 3</option>
-      </select>
-      <button onClick={handleButtonClick}>Add Item</button>
-      <ul>
-        {listItems.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    useEffect(() => {
+        axios.get('https://54.177.181.151:4000/listDBs')
+          .then(response => {
+            setData(response.data);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, []);
+
+    return (
+        <div className="dropdown">
+            <div className="dropdown-menu" onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
+                DataBase:
+                {state ?(<ul className="dropdown-list" onMouseEnter={showDropdown}>
+                    <li>
+                        <div className="button">
+                            {Button && <Button buttonStyle='btn--outline' buttonSize='btn--small'> Collection 1
+                            </Button>}
+                        </div>
+                    </li>
+                    <li>
+                        <div className="button">
+                            {Button && <Button buttonStyle='btn--outline' buttonSize='btn--small'> Collection 2
+                            </Button>}
+                        </div>
+                    </li>
+                    <li>
+                        <div className="button">
+                            {Button && <Button buttonStyle='btn--outline' buttonSize='btn--small'> Collection 3
+                            </Button>}
+                        </div>
+                    </li>
+                    <li>
+                        <div className="button">
+                            {Button && <Button buttonStyle='btn--outline' buttonSize='btn--small'> Collection 4
+                            </Button>}
+                        </div>
+                    </li>
+                </ul>):
+                null}
+            </div>
+        </div>
+    )
 }
 
