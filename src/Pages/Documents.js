2886ef1971db2d32da8e8f39961008b8be72facc
thabinhl21 <binhle6345@gmail.com>
2023-06-01 12:23:08 -0700

    add logo to all pages

--- src/Pages/Documents.js
+++ src/Pages/Documents.js
@@ -3,7 +3,8 @@ import TextBoxDoc from '../Components/TextBoxDoc';
 import axios from "axios";
 import { Button } from "../Components/Button";
 import { useNavigate, useLocation } from 'react-router-dom';
-import './Documents.css'
+import './Documents.css';
+import Dinosaur from "../assets/Dinosaur.png";
 
 
 const Documents = () => {
@@ -82,6 +83,7 @@ const Documents = () => {
     <div className="Documents">
       <div className="container">
         <div className="left-collumn">
+        <img src={Dinosaur} id="dino-img" alt="dinosaur-logo"/>
           <h1>DinoDB</h1>
             <TextBoxDoc db_id={location.state.db_id} coll_id={location.state.coll_id} />
             
