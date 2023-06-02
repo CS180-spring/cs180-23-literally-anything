4dba6e460900eace792baff5e6571ae5761e49c3
Axiion8 <84153224+Axiion8@users.noreply.github.com>
2023-06-02 00:18:06 -0700

    reverting changes

--- src/Pages/ViewSignIn.js
+++ src/Pages/ViewSignIn.js
@@ -1,7 +1,24 @@
 import React, { createContext, useState, useEffect, } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { Button } from '../Components/Button';
+import TextBox from '../Components/TextBox';
 import Dinosaur from "../assets/Dinosaur.png";
+
+const ViewSignIn = () => {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    axios.get('https://54.177.181.151:4000/listDBs')
+      .then(response => {
+        setData(response.data); //fills information of db into array that'll be used later for a table
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
   const handleRowClick = (item) => {
     console.log('Clicked row:', item); //used to track what row is clicked when interacting with table
@@ -28,6 +45,14 @@ import Dinosaur from "../assets/Dinosaur.png";
     navigate('/Collections', {state:{id:item.id}}); //navigate to collections page
   }
 
+  const tdStyle = { //in line styling
+    textAlign: 'center'
+  };
+
+  const thStyle = {
+    textAlign: 'center'
+  }
+
   return (
 
       <div className="Databases">
@@ -35,10 +60,40 @@ import Dinosaur from "../assets/Dinosaur.png";
           <div className="left_column">
             <img src={Dinosaur} id="dino-img" alt="dinosaur-logo"/>
             <h1>DinoDB+</h1>
+            <TextBox/>
+            
           </div>
 
           <div className="main_content">
-
+            <div className="table_container">
+              <table>
+                <thead>
+                  <tr> 
+                    <th>DataBase ID</th>
+                    <th style={thStyle}>DataBase Name</th>
+                    <th style={thStyle}>View Collections or Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((item) => ( //inputting data from array into table
+                  <tr key={item.id}>
+                    <td> {item.id} </td>
+                    <td style={tdStyle}> {item.name} </td>
+                    <td style={tdStyle}>
+                    {Button && //collections navigation button
+                      <Button buttonStyle='btn--outline' buttonSize='btn--xtrasmall' onClick={() => {handleCollections(item)}}>
+                        Collections
+                      </Button>}
+                    {Button && //delete db button
+                      <Button buttonStyle='btn--outline' buttonSize='btn--xtrasmall' onClick={() => {handleDelete(item)}}> 
+                        DeleteDB
+                      </Button>}
+                    </td>
+                  </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
