64d46c23747a84ef551d8619e3f7d49f3713b8a2
Axiion8 <84153224+Axiion8@users.noreply.github.com>
2023-06-02 00:11:27 -0700

    clearing up comments

--- src/backend/db/Encryption.cpp
+++ src/backend/db/Encryption.cpp
@@ -17,34 +17,21 @@ public:
     void encrypt();
     void decrypt();
 };
- 
-// Definition of encryption function
+
 void encdec::encrypt()
 {
-    // Key to be used for encryption
     cout << "key: ";
     cin >> key;
  
-    // Input stream
     fstream fin, fout;
- 
-    // Open input file
-    // ios::binary- reading file
-    // character by character
     fin.open(file, fstream::in);
     fout.open("encrypt.txt", fstream::out);
  
-    // Reading original file till
-    // end of file
     while (fin >> noskipws >> c) {
         int temp = (c + key);
- 
-        // Write temp as char in
-        // output file
         fout << (char)temp;
     }
  
-    // Closing both files
     fin.close();
     fout.close();
 }
